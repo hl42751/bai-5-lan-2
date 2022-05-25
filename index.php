ac4e37ce09ec0e96e23d48137da2a0265c037f00
hl42751 <104617618+hl42751@users.noreply.github.com>
2022-05-25 15:36:25 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!-- Giai phuong trinh bac nhat ax + b =0; -->
+
+<?php 
+$a = 5;
+$b = 6;
+if($a == 0){
+    if($b == 0){
+        echo 'Phuong trinh co vo so nghiem';
+    }else{
+        echo 'Phuong trinh vo nghiem';
+    }
+}else{
+    $x = - $b / $a;
+    echo "Pt co nghiem: $x";
+}
+?>
+
+<!-- VD2 -->
+<!-- <?php 
+$t = date('H');
+if($t < '10'){
+    echo 'Have a good morning';
+}elseif($t < '20'){
+    echo 'have a good day';
+}else{
+    echo 'Have a good night';
+}
+echo 'br';
+var_dump($t);
+?> -->
